09eac7781dce051c372011c66601cf70cb0c1298
Alex Felici <sandroms521@gmail.com>
2025-02-09 23:09:30 -0500

    Trying Figma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,18 @@ export default function Home() {
 
   return (
     <div className="container">
-      <button className="counter" onClick={handleClick}>
-        <p>Click Me: {count}</p>
+      <div className="z-10 text-white font-['Inter']">Click Me: {count}</div>
+      <button
+        onClick={handleClick}
+        className="z-0 w-[200px] h-[200px] absolute bg-[#2b2b2b] rounded-[15px] shadow-[inset_0px_0px_4px_0px_rgba(78,168,94,0.25)] flex-col justify-start items-start inline-flex overflow-hidden"
+      >
+        <div className="w-[114px] h-[114px] p-2.5 justify-start items-center gap-2.5 inline-flex">
+          <div className="w-[134px] h-[134px] bg-[#50d39d] rounded-full blur-[100px]"></div>
+        </div>
+        <div className="p-2.5 justify-center items-center gap-2.5 inline-flex"></div>
+        <div className="w-[153px] h-[153px] p-2.5 justify-start items-center gap-2.5 inline-flex">
+          <div className="w-[180px] h-[180px] bg-[#4da85e] rounded-full blur-[100px]"></div>
+        </div>
       </button>
     </div>
   );
