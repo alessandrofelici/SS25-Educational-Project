7057c1e754ea4c4b4bd42bb64d4a07c1d9ab1f4c
Alex Felici <sandroms521@gmail.com>
2025-02-09 21:51:22 -0500

    Added Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+"use client";
 import { useState } from "react";
 
 export default function Home() {
-  const [count,setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count+1)
-  }
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
   return (
-    <>
-      <div className="container">
-        <button className="counter" onClick={(handleClick)}>Click Me: {count}</button>
-      </div>
-    </>
+    <div className="container">
+      <button className="counter" onClick={handleClick}>
+        <p>Click Me: {count}</p>
+      </button>
+    </div>
   );
 }
