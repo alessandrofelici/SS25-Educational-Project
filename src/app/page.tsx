35a897e5022019a531c3b23c3b7ee77b4e616331
Alex Felici <sandroms521@gmail.com>
2025-02-22 20:21:31 -0500

    Minor Fixes to Ollama Api Calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,37 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  const [prompt, setPrompt] = useState("initial phrase");
+  const [response, setResponse] = useState("no response");
 
-  const handleClick = () => {
-    setCount(count + 1);
+  const sendPrompt = async () => {
+    const res = await fetch("/api/ollama", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ prompt }),
+    });
+
+    const data = await res.json();
+    setResponse(data.response);
   };
 
   return (
     <div className="container">
-      <div className="z-10 text-white font-['Inter']">Click Me: {count}</div>
-      <button
-        onClick={handleClick}
-        className="z-0 w-[200px] h-[200px] absolute bg-[#2b2b2b] rounded-[15px] shadow-[inset_0px_0px_4px_0px_rgba(78,168,94,0.25)] flex-col justify-start items-start inline-flex overflow-hidden"
+      <textarea
+        className="searchBar"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
       >
-        <div className="w-[114px] h-[114px] p-2.5 justify-start items-center gap-2.5 inline-flex">
-          <div className="w-[134px] h-[134px] bg-[#50d39d] rounded-full blur-[100px]"></div>
-        </div>
-        <div className="p-2.5 justify-center items-center gap-2.5 inline-flex"></div>
-        <div className="w-[153px] h-[153px] p-2.5 justify-start items-center gap-2.5 inline-flex">
-          <div className="w-[180px] h-[180px] bg-[#4da85e] rounded-full blur-[100px]"></div>
-        </div>
-      </button>
+        <input type="text" placeholder="Please Enter Prompt..." />
+      </textarea>
+      <div>
+        <button className="button" onClick={sendPrompt}>
+          Submit
+        </button>
+      </div>
+      <div>
+        <p>{response}</p>
+      </div>
     </div>
   );
 }
