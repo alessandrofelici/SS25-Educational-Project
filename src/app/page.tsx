91d2ea5d1b716112e23f898afc3b95e191812713
Alex Felici <sandroms521@gmail.com>
2025-03-18 22:52:32 -0400

    Week 5 File Upload Outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Home() {
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("no response");
+  const [fileName, setFileName] = useState("");
 
   const sendPrompt = async () => {
     setResponse("Waiting...");
@@ -17,8 +18,41 @@ export default function Home() {
     setResponse(data.response);
   };
 
+  const handleFileChange = async (e) => {
+    console.log(fileName);
+    const file = e.target.files[0];
+    if (!file) return;
+
+    setFileName(file.name);
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const content = event.target.result;
+    };
+  };
+
+  const handleClear = () => {};
+
   return (
     <div className="container">
+      <div>
+        <h1>Ollama Chat</h1>
+      </div>
+      <div>
+        <input
+          id="fileItem"
+          type="file"
+          accept=".txt,.md,.pdf"
+          onClick={handleFileChange}
+        ></input>
+      </div>
+      <div>
+        <button className="button" onClick={handleClear}>
+          Clear File
+        </button>
+      </div>
+      <div className="border p-2 w-[200px] h-[50px]">chatbot text here</div>
       <textarea
         className="searchBar"
         value={prompt}
@@ -37,3 +71,16 @@ export default function Home() {
     </div>
   );
 }
+
+// TO DO:
+
+// Main task:
+// read FileReader docs to see how to open file selection?
+
+// Front end:
+// Center Items, are divs necessary?
+// and/or reorganize by using more tailwind
+// General front end appearance
+
+// Additional time:
+// Later: try to redo without nextjs
